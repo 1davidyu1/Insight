98a7f2d9a175d1a66d849e12169699342d0ed94c
1davidyu1 <yudavid@msu.edu>
2024-04-13 19:12:42 -0400

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const Page = () => {
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[600px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className="flex max-h-full max-w-full items-center justify-center mb-36">
                 <Image 
@@ -63,8 +63,8 @@ const Page = () => {
           </div>
           {chats.map(({ id, image_file, prompt, output, audio_file }) => (
             <Link
-              key={id}
-              href={`/?photoId=${id}`}
+              key={id}  
+              href={`/chat/${id}`}
               // shallow
               className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
             >
